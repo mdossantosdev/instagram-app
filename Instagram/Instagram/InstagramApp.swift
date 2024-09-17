dc9486cd20321589a0968d587c536bc5369387ea
Marc Dos Santos <mdossantosdev@gmail.com>
2024-09-18 00:13:20 +0200

    Init Instagram App

---
+++ Instagram/Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Marc on 18/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
