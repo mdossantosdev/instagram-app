997d395a4a549872bdb359c9699d12f69b773e23
Marc Dos Santos <mdossantosdev@gmail.com>
2024-09-18 22:21:19 +0200

    Add MainTabView

---
+++ Instagram/Instagram/Core/TabBar/MainTabView.swift
@@ -0,0 +1,44 @@
+//
+//  MainTabView.swift
+//  Instagram
+//
+//  Created by Marc on 18/09/2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            Text("Feed")
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            
+            Text("Search")
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            
+            Text("Upload Post")
+                .tabItem {
+                    Image(systemName: "plus.square.fill")
+                }
+            
+            Text("Notifications")
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            
+            Text("Profile")
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .tint(.black)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
