657f061c3a4cc8ca5932c2d015a5a8a994315919
Marc Dos Santos <mdossantosdev@gmail.com>
2024-09-19 23:46:07 +0200

    Add NavigationStack and toolbar to ProfileView

--- Instagram/Instagram/Core/Profile/ProfileView.swift
+++ Instagram/Instagram/Core/Profile/ProfileView.swift
@@ -16,75 +16,90 @@ struct ProfileView: View {
     ]
 
     var body: some View {
-        ScrollView {
-            VStack(spacing: 10) {
-                HStack {
-                    AsyncImage(url: URL(string: "https://i.pravatar.cc/300?img=12")) { image in
-                        image
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 80, height: 80)
-                            .clipShape(Circle())
-                    } placeholder: {
-                        Image(systemName: "person.fill")
-                            .font(.system(size: 36))
-                            .frame(width: 80, height: 80)
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 10) {
+                    HStack {
+                        AsyncImage(url: URL(string: "https://i.pravatar.cc/300?img=12")) { image in
+                            image
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 80, height: 80)
+                                .clipShape(Circle())
+                        } placeholder: {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 36))
+                                .frame(width: 80, height: 80)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 40)
+                                        .stroke(.black, lineWidth: 1)
+                                )
+                        }
+                        
+                        Spacer()
+                        
+                        HStack(spacing: 8) {
+                            UserStatView(value: 3, title: "Posts")
+                            
+                            UserStatView(value: 26, title: "Followers")
+                            
+                            UserStatView(value: 34, title: "Following")
+                        }
+                    }
+                    .padding(.horizontal)
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("John Doe")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                        
+                        Text("New York")
+                            .font(.subheadline)
+                    }
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal)
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Edit Profile")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .frame(width: 360, height: 32)
+                            .foregroundStyle(.black)
                             .overlay(
-                                RoundedRectangle(cornerRadius: 40)
-                                    .stroke(.black, lineWidth: 1)
+                                RoundedRectangle(cornerRadius: 6)
+                                    .stroke(Color.gray, lineWidth: 1)
                             )
                     }
-
-                    Spacer()
-
-                    HStack(spacing: 8) {
-                        UserStatView(value: 3, title: "Posts")
-
-                        UserStatView(value: 26, title: "Followers")
-
-                        UserStatView(value: 34, title: "Following")
+                    
+                    Divider()
+                }
+                
+                LazyVGrid(columns: gridItems, spacing: 2) {
+                    ForEach(0 ... 15, id: \.self) { _ in
+                        AsyncImage(url: URL(string: "https://i.pravatar.cc/300?img=10")) { image in
+                            image
+                                .resizable()
+                                .scaledToFill()
+                        } placeholder: {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 36))
+                                .frame(width: 80, height: 80)
+                        }
                     }
                 }
-                .padding(.horizontal)
-
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("John Doe")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-
-                    Text("New York")
-                        .font(.subheadline)
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
-
-                Button {
-
-                } label: {
-                    Text("Edit Profile")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .frame(width: 360, height: 32)
-                        .foregroundStyle(.black)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 6)
-                                .stroke(Color.gray, lineWidth: 1)
-                        )
-                }
-
-                Divider()
             }
-
-            LazyVGrid(columns: gridItems, spacing: 2) {
-                ForEach(0 ... 15, id: \.self) { _ in
-                    AsyncImage(url: URL(string: "https://i.pravatar.cc/300?img=10")) { image in
-                        image
-                            .resizable()
-                            .scaledToFill()
-                    } placeholder: {
-                        Image(systemName: "person.fill")
-                            .font(.system(size: 36))
-                            .frame(width: 80, height: 80)
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "line.3.horizontal")
+                            .foregroundStyle(.black)
+                            .fontWeight(.semibold)
                     }
                 }
             }
