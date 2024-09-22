d386e2255e98e99403dc0d73f890f60011c58c04
Marc Dos Santos <mdossantosdev@gmail.com>
2024-09-22 17:11:14 +0200

    Add ProfileHeaderView and use it in CurrentUserProfileView and ProfileView

--- Instagram/Instagram/Core/Profile/CurrentUserProfileView.swift
+++ Instagram/Instagram/Core/Profile/CurrentUserProfileView.swift
@@ -18,68 +18,7 @@ struct CurrentUserProfileView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
-                VStack(spacing: 10) {
-                    HStack {
-                        AsyncImage(url: URL(string: user.profileImageUrl ?? "")) { image in
-                            image
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 80, height: 80)
-                                .clipShape(Circle())
-                        } placeholder: {
-                            Image(systemName: "person.fill")
-                                .font(.system(size: 36))
-                                .frame(width: 80, height: 80)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 40)
-                                        .stroke(.black, lineWidth: 1)
-                                )
-                        }
-
-                        Spacer()
-
-                        HStack(spacing: 8) {
-                            UserStatView(value: 3, title: "Posts")
-
-                            UserStatView(value: 26, title: "Followers")
-
-                            UserStatView(value: 34, title: "Following")
-                        }
-                    }
-                    .padding(.horizontal)
-
-                    VStack(alignment: .leading, spacing: 4) {
-                        if let fullname = user.fullname {
-                            Text(fullname)
-                                .font(.subheadline)
-                                .fontWeight(.semibold)
-                        }
-
-                        if let bio = user.bio {
-                            Text(bio)
-                                .font(.subheadline)
-                        }
-
-                    }
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.horizontal)
-
-                    Button {
-
-                    } label: {
-                        Text("Edit Profile")
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                            .frame(width: 360, height: 32)
-                            .foregroundStyle(.black)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 6)
-                                    .stroke(Color.gray, lineWidth: 1)
-                            )
-                    }
-
-                    Divider()
-                }
+                ProfileHeaderView(user: user)
 
                 PostGridView(posts: posts)
             }
--- Instagram/Instagram/Core/Profile/ProfileView.swift
+++ Instagram/Instagram/Core/Profile/ProfileView.swift
@@ -17,56 +17,7 @@ struct ProfileView: View {
 
     var body: some View {
         ScrollView {
-            VStack(spacing: 10) {
-                HStack {
-                    AsyncImage(url: URL(string: user.profileImageUrl ?? "")) { image in
-                        image
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 80, height: 80)
-                            .clipShape(Circle())
-                    } placeholder: {
-                        Image(systemName: "person.fill")
-                            .font(.system(size: 54))
-                            .scaledToFit()
-                            .frame(width: 80, height: 80)
-                            .clipShape(Circle())
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 40)
-                                    .stroke(.black, lineWidth: 1)
-                            )
-                    }
-
-                    Spacer()
-
-                    HStack(spacing: 8) {
-                        UserStatView(value: 3, title: "Posts")
-
-                        UserStatView(value: 26, title: "Followers")
-
-                        UserStatView(value: 34, title: "Following")
-                    }
-                }
-                .padding(.horizontal)
-
-                VStack(alignment: .leading, spacing: 4) {
-                    if let fullname = user.fullname {
-                        Text(fullname)
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                    }
-
-                    if let bio = user.bio {
-                        Text(bio)
-                            .font(.footnote)
-                            .multilineTextAlignment(.leading)
-                    }
-                }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(.horizontal)
-
-                Divider()
-            }
+            ProfileHeaderView(user: user)
 
             PostGridView(posts: posts)
         }
