3328c43ea9c6da19883ab5b8758f6331702264c4
Marc Dos Santos <mdossantosdev@gmail.com>
2024-09-23 13:14:32 +0200

    Init AuthService

---
+++ Instagram/Instagram/Core/Authentication/Service/AuthService.swift
@@ -0,0 +1,19 @@
+//
+//  AuthService.swift
+//  Instagram
+//
+//  Created by Marc on 23/09/2024.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthService {
+    @Published var userSession: FirebaseAuth.User?
+    
+    static let shared = AuthService()
+    
+    init() {
+        self.userSession = Auth.auth().currentUser
+    }
+}
