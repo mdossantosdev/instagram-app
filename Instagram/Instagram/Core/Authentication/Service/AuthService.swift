7ae9af872a95ec071a31ae19117846b989316429
Marc Dos Santos <mdossantosdev@gmail.com>
2024-09-25 21:55:32 +0200

    Add uploadUserData method in AuthService

--- Instagram/Instagram/Core/Authentication/Service/AuthService.swift
+++ Instagram/Instagram/Core/Authentication/Service/AuthService.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseFirestore
 
 class AuthService {
     @Published var userSession: FirebaseAuth.User?
@@ -22,6 +23,8 @@ class AuthService {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
             self.userSession = result.user
+
+            await uploadUserData(uid: result.user.uid, username: username, email: email)
         } catch {
             print("DEBUG: Failed to register user with error \(error.localizedDescription)")
         }
@@ -40,5 +43,13 @@ class AuthService {
     func signout() {
         try? Auth.auth().signOut()
         self.userSession = nil
+    }
+
+    private func uploadUserData(uid: String, username: String, email: String) async {
+        let user = User(id: uid, username: username, email: email)
+        guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
+
+        try? await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
+
     }
 }
