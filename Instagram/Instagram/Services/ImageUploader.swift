62820bc6012b8f072136f0438d940ed9225fd52b
Marc Dos Santos <mdossantosdev@gmail.com>
2024-10-14 23:33:08 +0200

    Add ImageUploader class with static method uploadImage

---
+++ Instagram/Instagram/Services/ImageUploader.swift
@@ -0,0 +1,27 @@
+//
+//  ImageUploader.swift
+//  Instagram
+//
+//  Created by Marc on 10/10/2024.
+//
+
+import FirebaseStorage
+import SwiftUI
+
+class ImageUploader {
+    static func uploadImage(image: UIImage) async throws -> String? {
+        guard let imageData = image.jpegData(compressionQuality: 0.5) else { return nil }
+        let filename = UUID().uuidString
+        let ref = Storage.storage().reference(withPath: "/profile_images/\(filename)")
+        
+        do {
+            let _ = try await ref.putDataAsync(imageData)
+            let url = try await ref.downloadURL()
+            
+            return url.absoluteString
+        } catch {
+            print("DEBUG: Failed to upload image with error \(error.localizedDescription)")
+            return nil
+        }
+    }
+}
