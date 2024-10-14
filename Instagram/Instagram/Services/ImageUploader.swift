b22fa6f921854eec993c11fdfea94c404243ec74
Marc Dos Santos <mdossantosdev@gmail.com>
2024-10-15 00:56:03 +0200

    Add uploadImage logic to updateUserData method from EditProfileViewModel

--- Instagram/Instagram/Services/ImageUploader.swift
+++ Instagram/Instagram/Services/ImageUploader.swift
@@ -13,11 +13,11 @@ class ImageUploader {
         guard let imageData = image.jpegData(compressionQuality: 0.5) else { return nil }
         let filename = UUID().uuidString
         let ref = Storage.storage().reference(withPath: "/profile_images/\(filename)")
-        
+
         do {
             let _ = try await ref.putDataAsync(imageData)
             let url = try await ref.downloadURL()
-            
+
             return url.absoluteString
         } catch {
             print("DEBUG: Failed to upload image with error \(error.localizedDescription)")
