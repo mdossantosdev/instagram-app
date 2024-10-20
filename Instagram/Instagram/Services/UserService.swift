50ae6cf5f2e6f14585bf932d6cd9e62ece673a00
Marc Dos Santos <mdossantosdev@gmail.com>
2024-10-20 18:33:32 +0200

    Add fetchUser method to UserService, update fetchPosts method from FeedViewModel and loadUserData method from AuthService

--- Instagram/Instagram/Services/UserService.swift
+++ Instagram/Instagram/Services/UserService.swift
@@ -9,11 +9,15 @@ import Foundation
 import Firebase
 
 class UserService {
-
     static func fetchUsers() async throws -> [User] {
         let snapshot = try await Firestore.firestore().collection("users").getDocuments()
         let documents = snapshot.documents
 
         return documents.compactMap({ try? $0.data(as: User.self) })
+    }
+
+    static func fetchUser(withUid uid: String) async throws -> User {
+        let snapshot = try await Firestore.firestore().collection("users").document(uid).getDocument()
+        return try snapshot.data(as: User.self)
     }
 }
