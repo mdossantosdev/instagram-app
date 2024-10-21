0f4f91119bc3231cd098bff4c7bc973181fe7393
Marc Dos Santos <mdossantosdev@gmail.com>
2024-10-21 18:31:12 +0200

    Add PostGridViewModel to PostGridView and modify posts attribute from PostGridView to user

--- Instagram/Instagram/Core/Components/ViewModel/PostGridViewModel.swift
+++ Instagram/Instagram/Core/Components/ViewModel/PostGridViewModel.swift
@@ -10,17 +10,17 @@ import Foundation
 class PostGridViewModel: ObservableObject {
     @Published var posts = [Post]()
     private let user: User
-    
+
     init(user: User) {
         self.user = user
-        
+
         Task { try await fetchUserPosts() }
     }
-    
+
     @MainActor
     func fetchUserPosts() async throws {
         self.posts = try await PostService.fetchUserPosts(uid: user.id)
-        
+
         for index in 0 ..< posts.count {
             posts[index].user = self.user
         }
